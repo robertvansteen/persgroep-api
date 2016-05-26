0d85fe8002d462f357ec061d4a335336d4afba33
Robert van Steen <studio@rovansteen.nl>
2016-05-26 12:32:36 +0200

    Transform ModelNotFoundException in to a 404

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use API;
+use Illuminate\Http\Response;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        API::error(function(ModelNotFoundException $exception)
+        {
+            return Response::create(['error' => 'Model not found.'], 404);
+        });
     }
 
     /**
