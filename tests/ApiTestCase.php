e9cf92223bf187876bd191ab7f5add5b7d1470b0
Robert van Steen <studio@rovansteen.nl>
2016-06-18 17:23:45 +0200

    Add api test case back again.

---
+++ tests/ApiTestCase.php
@@ -0,0 +1,67 @@
+<?php
+
+use App\User;
+
+class ApiTestCase extends TestCase
+{
+	protected $headers = [];
+
+	/**
+	 * Authenticate a user for testing purposes.
+	 *
+	 * @param String userId
+	 * @return this
+	 */
+	public function authenticate($userId = 1)
+	{
+		$user = User::find($userId);
+		$token = JWTAuth::fromUser($user);
+
+		$this->headers['Authorization'] = "Bearer: $token";
+
+		return $this;
+	}
+
+	/**
+	 * Make a get request.
+	 *
+	 * @param  String $uri
+	 * @param  Array $headers
+	 *
+	 * @return this
+	 */
+	public function get($uri, array $headers = [])
+	{
+		$headers = array_merge($headers, $this->headers);
+
+		return parent::get($this->getUri($uri), $headers);
+	}
+
+	/**
+	 * Make a post request.
+	 *
+	 * @param  String $uri
+	 * @param  Array $data
+	 * @param  Array $headers
+	 *
+	 * @return this
+	 */
+	public function post($uri, array $data = [], array $headers = [])
+	{
+		$headers = array_merge($headers, $this->headers);
+
+		return parent::post($this->getUri($uri), $data, $headers);
+	}
+
+	/**
+	 * Get URI with API prefix.
+	 *
+	 * @param String $uri
+	 * @return String
+	 */
+	protected function getUri($uri)
+	{
+		return '/' . env('API_PREFIX') . $uri;
+	}
+
+}
