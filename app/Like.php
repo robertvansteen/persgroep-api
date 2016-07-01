b94e812e072e077fb723b4bf1b08492834090486
Robert van Steen <studio@rovansteen.nl>
2016-07-01 08:07:45 +0200

    Add StoryLiked event and fire it whenever a story is liked.

--- app/Like.php
+++ app/Like.php
@@ -13,4 +13,35 @@ class Like extends Model
 		'user_id',
 		'story_id',
 	];
+
+	/**
+	 * The attributes that should be casted to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'id'       => 'string',
+		'story_id' => 'string',
+		'user_id'  => 'string',
+	];
+
+	protected $with = ['user', 'story'];
+
+	/**
+	 * Define the user relationship.
+	 *
+	 * @return \Illuminate\Database\Eloquent\BelongsTo
+	 */
+	public function user() {
+		return $this->belongsTo('App\User');
+	}
+
+	/**
+	 * Define the user relationship.
+	 *
+	 * @return \Illuminate\Database\Eloquent\BelongsTo
+	 */
+	public function story() {
+		return $this->belongsTo('App\Story');
+	}
 }
